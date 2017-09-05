eb6dbd0d7e2c42b5e48b8a0f3a1f8c8b9ae5af60
Leonard_tia <leo_zzpop@163.com>
2017-09-05 22:09:07 +0800

    Initial commit

---
+++ study_1.py
@@ -0,0 +1,13 @@
+import math
+x = input("x:")
+if x == 5:
+    print ("five")
+y = input("y:")
+if y == 10:
+    print ("ten")
+    #x的y次方
+print(x ** y)
+#开方
+z = math.sqrt(x ** y)
+print(z)
+print (z*z)
---
+++ study_2.py
@@ -0,0 +1,9 @@
+what_he_does = ' plays '
+his_name = 'Robert Johnson'
+his_instrument = ' guitar '
+artist_intro = his_name + what_he_does + his_instrument
+print (artist_intro)
+
+num = 1
+string = '1'
+print (type(string))
