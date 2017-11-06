6799cf57ed6a29c1db42babe8caaffd7dcbddd64
Leonard_tia <leo_zzpop@163.com>
2017-11-06 12:08:20 +0800

    练习

--- study_test.py
+++ study_test.py
@@ -1,1 +1,21 @@
-album = ['a','b','c','d','e','f','g']
+mm = 1
+ss = 60 * mm
+hh = 60 * ss
+dd = 24 * hh
+for i in range(1, 8):
+    # print('{0} day is {1} m,is {2} ss,is {3} hh hh'.format(i,i*dd,i*dd/60,i*dd/60/60))
+    pass
+k = input('please input qj:')
+c = 0.0041859 * eval(k)
+print(c)
+
+
+def abc():
+    sp = input('car speed(km/h):')
+    long = input('how long:')
+    time = round(eval(long) / eval(sp), 2)
+    print('will {} hours arealy'.format(time))
+    abc()
+
+
+abc()
