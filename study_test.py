a89c75918b7db0bd2547865ae498d55135142db5
Leonard_tia <leo_zzpop@163.com>
2017-11-10 13:15:33 +0800

    计算车速和耗时

--- study_test.py
+++ study_test.py
@@ -1,21 +1,49 @@
-mm = 1
-ss = 60 * mm
-hh = 60 * ss
-dd = 24 * hh
-for i in range(1, 8):
-    # print('{0} day is {1} m,is {2} ss,is {3} hh hh'.format(i,i*dd,i*dd/60,i*dd/60/60))
-    pass
-k = input('please input qj:')
-c = 0.0041859 * eval(k)
-print(c)
+s = 'my name is leonard'
+print(s[:])
+print(s[1:10:1])
+print(s[1:10:2])
 
+ss = '%s is ten %d'
+print(ss % ('leo', 4))
+t = ss.__add__(s)
+print(t)
+l = '1,2,3,4,5,6,7'
+ll = l.split(',')
+print(ll)
+lll = ','.join(ll)
+print(lll)
+j = 0
+for i in range(1, 101):
+    j = i + j
+print(j)
+j = 0
+for h in range(1, 1000):
+    if (h % 3 == 0) or (h % 5 == 0):
+        k = h
+        j = k + j
+        # print(k)
+print(j)
+s = [h for h in range(1, 1000000) if (h % 3 == 0) or (h % 5 == 0)]  # 列表表达式
+t = (h for h in range(1, 1000000) if (h % 3 == 0) or (h % 5 == 0))  # 生成器表达式
+print(sum(s))
+print(sum(t))
 
-def abc():
-    sp = input('car speed(km/h):')
-    long = input('how long:')
-    time = round(eval(long) / eval(sp), 2)
-    print('will {} hours arealy'.format(time))
-    abc()
+b = int(input('please input bingbao:'))
+s = []
+while b != 1:
+    if b % 2 == 0:
+        b = b / 2
+    else:
+        b = b * 3 + 1
+    s.append(b)
+print(s)
 
+m = [1, 2, 3, 4, 5, 6, 7]
+l = []
+for n in m:
+    l.append(n)
+print(l)
+print(id(m))
 
-abc()
+t = set(1, 2, 3, 4, 5)
+print(t)
