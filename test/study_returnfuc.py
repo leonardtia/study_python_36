ca9767a66a4b9b9f098120b79d142bf4e25b6fe4
Leonard_tia <leo_zzpop@163.com>
2017-11-20 14:34:28 +0800

    高阶函数:闭包练习，利用闭包返回一个计数器函数，每次调用它返回递增整数：

---
+++ test/study_returnfuc.py
@@ -0,0 +1,37 @@
+'''
+利用闭包返回一个计数器函数，每次调用它返回递增整数：
+'''
+
+
+def createCounter():
+    def ccc(j):
+        def fff():
+            return j + 1
+
+        return fff
+
+    fs = []
+    for i in range(0, 4):
+        fs.append(ccc(i))
+    return fs
+
+
+c1, c2, c3, c4 = createCounter()
+print(c1(), c2(), c3(), c4())
+
+
+def generate_counter():
+    CNT = [0]
+
+    def add_one():
+        CNT[0] = CNT[0] + 1
+        return CNT[0]
+
+    return add_one
+
+
+counter = generate_counter()
+print(counter())
+print(counter())
+print(counter())
+print(counter())
