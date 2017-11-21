def40ffac1a698fde799dbf75fb5ee37a26e4d32
Leonard_tia <leo_zzpop@163.com>
2017-11-21 16:06:23 +0800

    继承与多态

---
+++ test/study_oop_superclass.py
@@ -0,0 +1,40 @@
+'''
+继承与多态
+falemale，male，child都是person的继承，但是falemale和male又分别重写了函数face
+monkey不是person的继承，但是因为和person一样有face方法，所以可以当成同一种类型进行传递
+'''
+
+
+class person(object):
+    def face(self):
+        print('眼睛', '鼻子', '耳朵', '嘴', '眉毛')
+
+
+class falemale(person):
+    def face(self):
+        print('带眼镜', '大嘴巴')
+
+
+class male(person):
+    def face(self):
+        print('大眼睛', '小嘴巴')
+
+
+class child(person):
+    pass
+
+
+class monkey(object):
+    def face(self):
+        print('毛茸茸')
+
+
+def trueface(person):
+    person.face()
+
+
+trueface(person())
+trueface(falemale())
+trueface(male())
+trueface(child())
+trueface(monkey())
