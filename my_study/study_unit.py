be548390f923fa94126aa9771a115cf3f934294e
Leonard_tia <leo_zzpop@163.com>
2017-11-27 12:11:33 +0800

    单元测试的学习

---
+++ my_study/study_unit.py
@@ -0,0 +1,53 @@
+'''
+单元测试学习
+'''
+
+
+class Dict(dict):
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % item)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+import unittest
+
+
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_atterror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+
+if __name__ == '__main__':
+    unittest.main()
